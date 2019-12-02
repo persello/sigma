2e0d2ac7bd122fed1d844bd1ff1cd110c5cea4b0
Riccardo Persello <riccardo.persello@outlook.it>
2019-12-02 22:26:21 +0100

    Working menu prototype.

--- lib/main.dart
+++ lib/main.dart
@@ -30,11 +30,15 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
   AnimationController _controller;
+  bool _dragAccepted = false;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(duration: const Duration(seconds: 1), vsync: this);
+    _controller = AnimationController(duration: const Duration(milliseconds: 150), vsync: this);
+    _controller.addListener(() {
+      setState(() {});
+    });
   }
 
   @override
@@ -50,25 +54,58 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
       color: Theme.of(context).scaffoldBackgroundColor,
       child: Stack(
         children: <Widget>[
-          Material(
-            elevation: 16,
-            // borderRadius: BorderRadius.only(topLeft: Radius.circular(16), bottomLeft: Radius.circular(16)),
-            color: Theme.of(context).scaffoldBackgroundColor,
-            child: Scaffold(
-              backgroundColor: Colors.transparent,
-              appBar: AppBar(
-                backgroundColor: Colors.transparent,
-                brightness: Theme.of(context).brightness,
-                textTheme: Theme.of(context).textTheme,
-                iconTheme: Theme.of(context).iconTheme,
-                elevation: 0,
-                centerTitle: true,
-                title: Text('E'),
-                leading: IconButton(
-                  onPressed: () => {},
-                  icon: AnimatedIcon(
-                    icon: AnimatedIcons.menu_home,
-                    progress: _controller,
+          GestureDetector(
+            onHorizontalDragStart: (DragStartDetails details) {
+              if (_controller.value > 0.5 ? details.globalPosition.dx > _controller.value * 100 : details.globalPosition.dx < 30) {
+                _dragAccepted = true;
+              } else {
+                _dragAccepted = false;
+              }
+            },
+            onHorizontalDragUpdate: (DragUpdateDetails details) {
+              if (_dragAccepted) {
+                _controller.value += (details.delta.dx / 100);
+              }
+            },
+            onHorizontalDragEnd: (DragEndDetails details) {
+              if (_dragAccepted) {
+                print(details.velocity.pixelsPerSecond.dx);
+                if (details.velocity.pixelsPerSecond.dx == 0) {
+                  if (_controller.value >= 0.5)
+                    _controller.forward();
+                  else
+                    _controller.reverse();
+                } else if (details.velocity.pixelsPerSecond.dx > 0)
+                  _controller.forward();
+                else
+                  _controller.reverse();
+              }
+            },
+            child: Transform.translate(
+              offset: Offset(_controller.value * 100, 0),
+              child: Material(
+                elevation: 16,
+                // borderRadius: BorderRadius.only(topLeft: Radius.circular(16), bottomLeft: Radius.circular(16)),
+                color: Theme.of(context).scaffoldBackgroundColor,
+                child: Scaffold(
+                  backgroundColor: Colors.transparent,
+                  appBar: AppBar(
+                    backgroundColor: Colors.transparent,
+                    brightness: Theme.of(context).brightness,
+                    textTheme: Theme.of(context).textTheme,
+                    iconTheme: Theme.of(context).iconTheme,
+                    elevation: 0,
+                    centerTitle: true,
+                    title: Text('E'),
+                    leading: IconButton(
+                      onPressed: () {
+                        _controller.value > 0.5 ? _controller.reverse() : _controller.forward();
+                      },
+                      icon: AnimatedIcon(
+                        icon: AnimatedIcons.menu_arrow,
+                        progress: _controller,
+                      ),
+                    ),
                   ),
                 ),
               ),
