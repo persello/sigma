0afbdddbaeac0eb5ccc1fe58bdab028e678a7975
Riccardo Persello <riccardo.persello@outlook.it>
2019-12-02 19:55:48 +0100

    Custom side menu structure.

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,9 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       theme: ThemeData(
         brightness: Brightness.light,
-        primaryColor: Colors.white,
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
-        primaryColor: Colors.black,
       ),
       home: MyHomePage(title: 'Sigma'),
     );
@@ -30,26 +28,52 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(duration: const Duration(seconds: 1), vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     // Backdrop
     return Container(
       color: Theme.of(context).scaffoldBackgroundColor,
-      padding: EdgeInsets.only(top: 88),
       child: Stack(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              // AnimatedIcon(icon: AnimatedIcons.home_menu, progress: new Animation())
-            ],
-          ),
           Material(
             elevation: 16,
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(16), topRight: Radius.circular(16)),
-            color: Theme.of(context).primaryColor,
-            child: Container(),
-          )
+            // borderRadius: BorderRadius.only(topLeft: Radius.circular(16), bottomLeft: Radius.circular(16)),
+            color: Theme.of(context).scaffoldBackgroundColor,
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                backgroundColor: Colors.transparent,
+                brightness: Theme.of(context).brightness,
+                textTheme: Theme.of(context).textTheme,
+                iconTheme: Theme.of(context).iconTheme,
+                elevation: 0,
+                centerTitle: true,
+                title: Text('E'),
+                leading: IconButton(
+                  onPressed: () => {},
+                  icon: AnimatedIcon(
+                    icon: AnimatedIcons.menu_home,
+                    progress: _controller,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
